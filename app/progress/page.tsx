d58efcf1d3ffc772c3da0e982603345be5418b5d
rikuto-mania <rikutoland55@gmail.com>
2025-12-29 01:45:37 +0900

    [add]進捗ページ作成

---
+++ app/progress/page.tsx
@@ -0,0 +1,39 @@
+import Footer from "@/conponents/footer";
+import Header from "@/conponents/header"
+import Highlight from "@/conponents/highlight";
+
+
+//ダミーテストデータ
+import highlightData from "../../data/highlight.json";
+
+export default function Progress(){
+    return(
+        <div>
+            <Header />
+            <div className="overflow-hidden">
+                <div className="bg-main bottom-circle text-white text-center">
+                    <p className="text-4xl font-bold pb-8">ああああああ</p>
+                    <p className="text-xl">ああああああああああああああああああああああああああああああ</p>
+                </div>
+            </div>
+            <section>
+                <p className="text-4xl text-center font-semibold">HighRights</p>
+
+
+                {highlightData.map((item,index) =>{
+                    const direction = index % 2 === 0 ? "left" : "right";
+                    return(
+                        <Highlight 
+                        key={item.id}
+                        direction = {direction}
+                        title = {item.title}
+                        subTitle = {item.subtitle}
+                        description = {item.description}
+                        image={item.image}
+                    />
+                   )})}
+            </section>
+            <Footer />
+        </div>
+    );
+};
